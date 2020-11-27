97b064803895c0f68d20c4cd3e3107013982ae8d
DGvagabond <54526517+DGvagabond@users.noreply.github.com>
2020-11-26 21:28:08 -0600

    Included 999 support for RA commands

--- Commands/Infect.cs
+++ Commands/Infect.cs
@@ -1,11 +1,10 @@
-﻿using RemoteAdmin;
-using CommandSystem;
+﻿using CommandSystem;
 using System;
 using Exiled.API.Features;
-using UnityEngine;
 using Exiled.Permissions.Extensions;
 using SCP008X.Components;
 using scp035.API;
+using SCP999X.API;
 
 namespace SCP008X.Commands
 {
@@ -13,6 +12,7 @@ namespace SCP008X.Commands
     public class Infect : ICommand
     {
         private Player Pull035() => Scp035Data.GetScp035();
+        private Player Pull999() => SCP999API.GetScp999();
         public string Command { get; } = "infect";
 
         public string[] Aliases { get; } = null;
@@ -56,7 +56,19 @@ namespace SCP008X.Commands
             {
                 Log.Debug($"SCP-035, by Cyanox, is not installed. Skipping check.", SCP008X.Instance.Config.DebugMode);
             }
-            if(ply.ReferenceHub.TryGetComponent(out SCP008 s008))
+            try
+            {
+                if (ply.UserId == Pull999().UserId)
+                {
+                    response = "You can not infect SCP-999 hosts.";
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                Log.Debug($"SCP-999-X, by DGvagabond, is not installed. Skipping check.", SCP008X.Instance.Config.DebugMode);
+            }
+            if (ply.ReferenceHub.TryGetComponent(out SCP008 s008))
             {
                 response = "This player is already infected.";
                 return false;
