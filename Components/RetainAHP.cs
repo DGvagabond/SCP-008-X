c7170b2ea0956917d9046d83489475f4361235ed
DGvagabond <54526517+DGvagabond@users.noreply.github.com>
2021-01-03 19:16:49 -0600

    Updated to EXILED 2.1.22

---
+++ Components/RetainAHP.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+using Exiled.API.Extensions;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using MEC;
+using UnityEngine;
+
+namespace SCP008X
+{
+    public class RetainAhp : MonoBehaviour
+    {
+        private Player _ply;
+        private float _currentAhp = 0;
+        private CoroutineHandle _keepAhp;
+        
+        public void Awake()
+        {
+            _ply = Player.Get(gameObject);
+            _keepAhp = Timing.RunCoroutine(KeepAhp());
+            Exiled.Events.Handlers.Player.Hurting += WhenHurt;
+            Exiled.Events.Handlers.Player.ChangingRole += WhenChange;
+        }
+        public void OnDestroy()
+        {
+            _ply = null;
+            Timing.KillCoroutines(_keepAhp);
+            Exiled.Events.Handlers.Player.Hurting -= WhenHurt;
+            Exiled.Events.Handlers.Player.ChangingRole -= WhenChange;
+        }
+        public void WhenHurt(HurtingEventArgs e)
+        {
+            if (e.Target != _ply)
+                return;
+
+            if (_currentAhp > 0)
+                _currentAhp -= e.Amount;
+            else
+                _currentAhp = 0;
+        }
+
+        public void WhenChange(ChangingRoleEventArgs e)
+        {
+            if (e.Player != _ply)
+                return;
+    
+            if (e.NewRole.GetTeam() == Team.SCP)
+            {
+                Timing.KillCoroutines(_keepAhp);
+                _ply.AdrenalineHealth = e.NewRole == RoleType.Scp096 ? 500f : 0f;
+            }
+            else
+                Timing.RunCoroutine(KeepAhp());
+        }
+        private IEnumerator<float> KeepAhp()
+        {
+            for(; ; )
+            {
+                if (_ply.AdrenalineHealth <= _currentAhp)
+                    _ply.AdrenalineHealth = _currentAhp;
+                else
+                {
+                    if (_ply.AdrenalineHealth >= Scp008X.Instance.Config.MaxAhp)
+                    {
+                        _ply.AdrenalineHealth = Scp008X.Instance.Config.MaxAhp;
+                    }
+                    _currentAhp = _ply.AdrenalineHealth;
+                }
+
+                yield return Timing.WaitForSeconds(0.05f);
+            }
+        }
+    }
+}
