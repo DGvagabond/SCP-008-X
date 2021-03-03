09b71e57304c66efb05e144dbd8c48eef2976a3f
DGvagabond <54526517+DGvagabond@users.noreply.github.com>
2021-03-02 21:46:29 -0600

    Removed unnecessary human check

--- Extensions.cs
+++ Extensions.cs
@@ -2,45 +2,6 @@
 {
     public static class Extensions
     {
-        public static bool NonHuman(this RoleType role, bool onlyScPs)
-        {
-            if (onlyScPs)
-            {
-                switch (role)
-                {
-                    case RoleType.Scp049:
-                    case RoleType.Scp0492:
-                    case RoleType.Scp079:
-                    case RoleType.Scp096:
-                    case RoleType.Scp106:
-                    case RoleType.Scp173:
-                    case RoleType.Scp93953:
-                    case RoleType.Scp93989:
-                        return true;
-                    default:
-                        return false;
-                }
-            }
-            else
-            {
-                switch (role)
-                {
-                    case RoleType.Scp049:
-                    case RoleType.Scp0492:
-                    case RoleType.Scp079:
-                    case RoleType.Scp096:
-                    case RoleType.Scp106:
-                    case RoleType.Scp173:
-                    case RoleType.Scp93953:
-                    case RoleType.Scp93989:
-                    case RoleType.Spectator:
-                    case RoleType.None:
-                        return true;
-                    default:
-                        return false;
-                }
-            }
-        }
         public static bool Gun(this ItemType item)
         {
             switch (item)
