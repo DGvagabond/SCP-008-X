671c3b20d81d53a18cfb4c09a38a227aeacb856e
DGvagabond <54526517+DGvagabond@users.noreply.github.com>
2021-10-12 19:59:24 -0500

    Updated EXILED references

--- Scp008X.cs
+++ Scp008X.cs
@@ -15,7 +15,7 @@ namespace SCP008X
 
         public override string Author => "DGvagabond";
         public override string Name => "Scp008X";
-        public override Version Version { get; } = new Version(3, 0, 0, 0);
+        public override Version Version { get; } = new Version(3, 0, 0, 1);
         public override Version RequiredExiledVersion { get; } = new Version(3, 0, 0);
 
         private EventHandlers _events;
