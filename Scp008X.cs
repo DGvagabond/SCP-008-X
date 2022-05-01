8a3f0eb89a07aec3789d00b506702d1ad9896483
DGvagabond <54526517+DGvagabond@users.noreply.github.com>
2022-05-01 09:48:37 -0400

    5.2.1 already

--- Scp008X.cs
+++ Scp008X.cs
@@ -22,7 +22,7 @@ namespace SCP008X
         public override string Author => "DGvagabond";
         public override string Name => "Scp008X";
         public override Version Version { get; } = new Version(3, 1, 1, 0);
-        public override Version RequiredExiledVersion { get; } = new Version(5, 1, 3);
+        public override Version RequiredExiledVersion { get; } = new Version(5, 2, 1);
 
         private EventHandlers _events;
 
